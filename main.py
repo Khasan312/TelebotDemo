b5ce9016ba4da201318b85912eed0c12cd9f1e5e
Khasan <hasan.aliev.2000@mail.ru>
2022-03-09 11:52:03 +0600

    fourth push

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
 import markups as nav
+import random
+from markups import *
 from db import Database
 
 TOKEN = "5203636918:AAEDMv7gz3cfkN37s1CAZ8PfGE6kyZQ8rBc"
@@ -32,6 +34,7 @@ async def start(message: types.Message):
         )
         await bot.send_message(message.from_user.id, "Укажите ваш ник")
         await Form.nickname.set()
+        
 
     else:
         await bot.send_message(
@@ -39,6 +42,9 @@ async def start(message: types.Message):
             "Вы уже зарегистрированы",
             reply_markup=nav.mainMenu,
         )
+
+
+    
 
 
 @dp.message_handler(state=Form.nickname)
@@ -50,29 +56,57 @@ async def process_nickname(message: types.Message, state):
 
     await Form.next()
 
-
 @dp.message_handler(state=Form.birth_date)
 async def process_birthday(message: types.Message, state):
-    birth_date = message.text
-    await Form.next()
+    await bot.send_message(message.from_user.id, "введи свою позицию ")
+
+    keyboard = types.InlineKeyboardMarkup(row_width=2, text= 'hello')
+    btn1 = types.InlineKeyboardButton('СТАЖЕР', callback_data="1")
+    btn2 = types.InlineKeyboardButton('МЕНТОР',  callback_data="2")
+    keyboard.add(btn1, btn2)
+    await bot.send_message(message.from_user.id, reply_markup=keyboard)
+
+    
+
+    # birth_date = message.text
 
     # save client's birthday to db
-    db.set_date_of_birth(message.from_user.id, birth_date)
+    db.set_date_of_birth(message.from_user.id, message.text)
+
+    await Form.next()
+
+
 
 
 
 
 @dp.message_handler(state=Form.position)
 async def process_position(message: types.Message, state):
-    await bot.send_message(message.from_user.id, "What is your position?: ")
+    # await bot.send_message(message.from_user.id, "What is your position?: ")
+    # await Form.next()
+    db.set_position(message.from_user.id, message.text)
     await Form.next()
 
 
+# @dp.callback_query_handler(text='btnPosition')
+# async def position(message: types.Message):
+#     await bot.delete_message(message.from_user.id, message.message.message_id)
+#     await bot.send_message(message.from_user.id, "Я ЗДЕСЬ", reply_markup=nav.mainMenu)
+
     # save client's position to db
-    db.set_position(message.from_user.id, message.text)
+    # await Form.next()
 
     
     await bot.send_message(message.from_user.id, "Сделано")
+
+    # await Form.next()
+
+
+# @dp.message_handler(comamnds='Buttons')
+# async def url_command(message: types.Message):
+#     await message.answer('Buttons', reply_markup=urlkb)
+
+
 
 
 
@@ -84,8 +118,10 @@ async def bot_message(message: types.Message):
                 message.from_user.id
             )
             user_nickname = "Ваш ник: " + db.get_nickname(message.from_user.id)
+            position = 'Ваша позиция: ' + db.get_position(message.from_user.id)
             await bot.send_message(message.from_user.id, user_nickname)
             await bot.send_message(message.from_user.id, date_birth)
+            await bot.send_message(message.from_user.id, position)
 
         else:
             if db.get_signup(message.from_user.id) == "setnickname":
--- markups.py
+++ markups.py
@@ -1,7 +1,21 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from threading import main_thread
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 btnProfile = KeyboardButton("👨Профиль")
 btnSub = KeyboardButton("💻Меню")
 
+
+mainMenu = InlineKeyboardMarkup(row_width=2)
+btnPosition = InlineKeyboardButton(text='Кнопка', callback_data='btnPosition')
+
+
+mainMenu.insert(btnPosition)
+
+
+
+
+
 mainMenu = ReplyKeyboardMarkup(resize_keyboard=True)
-mainMenu.add(btnProfile, btnSub)
+mainMenu.add(btnProfile, btnSub, btnPosition)
+
+
